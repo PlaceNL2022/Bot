77622591353e132aa685cc2c6151618589e8d277
Sven <sven.bluethoot@gmail.com>
2022-04-01 22:46:37 +0200

    Add new reddit to the match tags

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -5,6 +5,7 @@
 // @description  De bot voor PlaceNL!
 // @author       NoahvdAa
 // @match        https://www.reddit.com/r/place/*
+// @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
