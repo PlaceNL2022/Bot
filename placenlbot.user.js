86d0052edd4fce64b4831bf25a04354453072c91
Noah van der Aa <ndvdaa@gmail.com>
2022-04-02 01:37:50 +0200

    bump version

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceNL Bot
 // @namespace    https://github.com/PlaceNL/Bot
-// @version      3
+// @version      4
 // @description  De bot voor PlaceNL!
 // @author       NoahvdAa
 // @match        https://www.reddit.com/r/place/*
