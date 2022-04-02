28983106b963e729804d976bd983ce6be12ec09d
fuho <ondrej.dolejsi@gmail.com>
2022-04-02 19:43:28 +0200

    Make TOKEN getter installable, README fixes, tampermonkey script in subdir. (#1)
    * Moved tampermonkey scripts to own subdir and updated README.md
    
    * formatting
    
    * Fixed Tampermonkey TOKEN instructions in README.md
    
    (cherry picked from commit 5dc3d3cf791277580e1c25387ad1eb44ab6a400a)

--- tampermonkey/placenlbot.user.js
+++ tampermonkey/placenlbot.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         PlaceNL Bot (Czech Edition)
 // @namespace    https://github.com/PlaceCZ/Bot
-// @version      10
+// @version      8
 // @description  Bot pro PlaceNL, předelán do češtiny
 // @author       NoahvdAa, GravelCZ, MartinNemi03
 // @match        https://www.reddit.com/r/place/*
-// @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
