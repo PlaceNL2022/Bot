6859cd53aaf4a7177fb6ed15a1bbd7a967b6090f
WaveLink <wavelink@email.cz>
2022-04-02 19:36:19 +0200

    Obedbrat starej soubor

--- BotInstaller.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir PlaceCZ
-cd PlaceCZ
-curl https://raw.githubusercontent.com/PlaceCZ/Bot/master/headlessBot.js -o bot.js
-curl https://raw.githubusercontent.com/PlaceCZ/Bot/master/package.json -o package.json
-npm install
