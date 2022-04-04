80edfa325e2d34c44dce9fde40b731b4c9fe38d9
Sheepolution <danielhaazen@hotmail.com>
2022-04-04 05:13:26 +0200

    Add connect tag to prevent cross origin question

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -14,6 +14,7 @@
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.xmlHttpRequest
+// @connect      https://reddit.com
 // ==/UserScript==
 
 // Sorry voor de rommelige code, haast en clean gaatn iet altijd samen ;)
