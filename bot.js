fe8dd40d40ce89a586ff39f7180fb1033867a49e
Noah van der Aa <ndvdaa@gmail.com>
2022-04-04 08:00:03 +0200

    bump version

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import getPixels from "get-pixels";
 import WebSocket from 'ws';
 
-const VERSION_NUMBER = 6;
+const VERSION_NUMBER = 7;
 
 console.log(`PlaceNL headless client V${VERSION_NUMBER}`);
 
